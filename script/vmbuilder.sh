328d3bfe4132ff04108a373e140260529ee0e505
scott.choi <hho.choi@kt.com>
2013-09-25 09:32:47 +0900

    [Issue #2] use 'generic' flavor for the precise base image

--- script/vmbuilder.sh
+++ script/vmbuilder.sh
@@ -6,7 +6,7 @@ SUDOERS_TMPL=/etc/vmbuilder/ubuntu/sudoers.tmpl
 grep -q -e 'ubuntu' $SUDOERS_TMPL || sudo sed -i '$a ubuntu ALL=(ALL) NOPASSWD:ALL\n#includedir /etc/sudoers.d' $SUDOERS_TMPL
 
 # flavour : virtual, server, generic
-sudo vmbuilder kvm ubuntu --suite precise --arch amd64 --flavour server  \
+sudo vmbuilder kvm ubuntu --suite precise --arch amd64 --flavour generic \
                --timezone Asia/Seoul --ssh-user-key ~/.ssh/id_rsa.pub    \
                --mirror http://ftp.daum.net/ubuntu --addpkg=vim          \
                --addpkg=openssh-server --addpkg=command-not-found        \
